4f65701401212dde65776f3b1f4d351bacec8dba
Nguyen Le <ldhnguyen9292@gmail.com>
2025-08-12 13:02:39 +0700

    Update eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true }
+      }
+    ]
+  }
+};
